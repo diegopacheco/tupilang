5002367ff8d62c974495aecd8f016206a504c73c
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-12 02:59:51 -0700

    tupic lang - added void type support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,40 @@
 import com.github.diegopacheco.tupilang.repl.REPL;
 
+import com.github.diegopacheco.tupilang.interpreter.Interpreter;
+import com.github.diegopacheco.tupilang.lexer.Lexer;
+import com.github.diegopacheco.tupilang.parser.Parser;
+import com.github.diegopacheco.tupilang.token.Token;
+import com.github.diegopacheco.tupilang.ast.Stmt;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String args[]) {
-        REPL.run(args);
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0].endsWith(".tupi")) {
+            runFile(args[0]);
+        } else {
+            REPL.run(args);
+        }
+    }
+
+    private static void runFile(String path) {
+        try {
+            String source = Files.readString(Paths.get(path));
+            Lexer lexer = new Lexer(source);
+            List<Token> tokens = lexer.tokenize();
+            Parser parser = new Parser(tokens);
+
+            List<Stmt> statements = parser.parse();
+            Interpreter interpreter = new Interpreter();
+            interpreter.interpret(statements);
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            System.exit(1);
+        } catch (Exception e) {
+            System.err.println("Error executing program: " + e.getMessage());
+            System.exit(1);
+        }
     }
 }
