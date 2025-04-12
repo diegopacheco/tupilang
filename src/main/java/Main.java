1db4bd4fbd4db5316f598e8bc6e19e63d89f2024
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-12 02:33:09 -0700

    tupic lang

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
+import com.github.diegopacheco.tupilang.repl.REPL;
 
-public class Main{
-  public static void main(String args[]){
-    System.out.println("It works!");
-  }
+public class Main {
+    public static void main(String args[]) {
+        REPL.run(args);
+    }
 }
