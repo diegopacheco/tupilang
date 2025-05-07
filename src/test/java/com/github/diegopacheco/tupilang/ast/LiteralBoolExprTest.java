6f5fb5b686099a2efa766ee7131ad6fb8ac7fa7c
diegopacheco <diego.pacheco.it@gmail.com>
2025-05-06 23:48:04 -0700

    Improve the lang - remove visitor refactoring

--- src/test/java/com/github/diegopacheco/tupilang/ast/LiteralBoolExprTest.java
+++ src/test/java/com/github/diegopacheco/tupilang/ast/LiteralBoolExprTest.java
@@ -6,15 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LiteralBoolExprTest {
 
     @Test
-    public void testConstructorAndValue() {
-        LiteralBoolExpr trueExpr = new LiteralBoolExpr(true);
-        LiteralBoolExpr falseExpr = new LiteralBoolExpr(false);
-
-        assertTrue(trueExpr.value);
-        assertFalse(falseExpr.value);
-    }
-
-    @Test
     public void testToStringTrue() {
         LiteralBoolExpr expr = new LiteralBoolExpr(true);
         assertEquals("true", expr.toString());
@@ -27,13 +18,20 @@ public class LiteralBoolExprTest {
     }
 
     @Test
-    public void testEquality() {
-        LiteralBoolExpr expr1 = new LiteralBoolExpr(true);
-        LiteralBoolExpr expr2 = new LiteralBoolExpr(true);
-        LiteralBoolExpr expr3 = new LiteralBoolExpr(false);
+    public void testCreation() {
+        LiteralBoolExpr trueExpr = new LiteralBoolExpr(true);
+        LiteralBoolExpr falseExpr = new LiteralBoolExpr(false);
 
-        // Test equality using the "value" field (not equals method)
-        assertEquals(expr1.value, expr2.value);
-        assertNotEquals(expr1.value, expr3.value);
+        assertTrue(trueExpr.getValue());
+        assertFalse(falseExpr.getValue());
+    }
+
+    @Test
+    public void testDirectInspection() {
+        LiteralBoolExpr trueExpr = new LiteralBoolExpr(true);
+        LiteralBoolExpr falseExpr = new LiteralBoolExpr(false);
+
+        assertTrue(trueExpr.getValue());
+        assertFalse(falseExpr.getValue());
     }
 }
