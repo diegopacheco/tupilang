1771e5c03ebf9b1fb3a676718e381b469774732b
diegopacheco <diego.pacheco.it@gmail.com>
2025-05-06 23:48:02 -0700

    Improve the lang - remove visitor refactoring

--- src/test/java/com/github/diegopacheco/tupilang/ast/ExpressionStatementTest.java
+++ src/test/java/com/github/diegopacheco/tupilang/ast/ExpressionStatementTest.java
@@ -19,45 +19,13 @@ public class ExpressionStatementTest {
     }
 
     @Test
-    public void testAcceptVisitor() {
+    public void testDirectInspection() {
         Expr expression = new LiteralIntExpr(99);
         ExpressionStatement stmt = new ExpressionStatement(expression);
 
-        StatementVisitor<String> visitor = new StatementVisitor<String>() {
-            @Override
-            public String visitValDeclaration(ValDeclaration stmt) {
-                return "";
-            }
-
-            @Override
-            public String visitPrintStatement(PrintStatement stmt) {
-                return "";
-            }
-
-            @Override
-            public String visitExpressionStatement(ExpressionStatement stmt) {
-                return "Expression: " + (stmt.getExpression() != null ?
-                        stmt.getExpression().toString() : "null");
-            }
-
-            @Override
-            public String visitFunctionDefinition(FunctionDefinition stmt) {
-                return "";
-            }
-
-            @Override
-            public String visitReturnStatement(ReturnStatement stmt) {
-                return "";
-            }
-
-            @Override
-            public String visitIfStatement(IfStatement stmt) {
-                return "";
-            }
-        };
-
-        String result = stmt.accept(visitor);
-        assertTrue(result.startsWith("Expression:"));
+        assertTrue(stmt.getExpression() instanceof LiteralIntExpr);
+        LiteralIntExpr intExpr = (LiteralIntExpr) stmt.getExpression();
+        assertEquals(99, intExpr.getValue());
     }
 
     @Test
