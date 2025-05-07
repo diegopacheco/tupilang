044d2b389cce3be2d88f4690ab9778a7ded1bac2
diegopacheco <diego.pacheco.it@gmail.com>
2025-05-07 00:43:16 -0700

    improve tupilang - added std func len: src/test/java/com/github/diegopacheco/tupilang/std/

---
+++ src/test/java/com/github/diegopacheco/tupilang/std/LenSTDTest.java
@@ -0,0 +1,59 @@
+package com.github.diegopacheco.tupilang.std;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LenSTDTest {
+
+    private LenSTD lenSTD;
+
+    @BeforeEach
+    public void setUp() {
+        lenSTD = new LenSTD();
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("len", lenSTD.getName());
+    }
+
+    @Test
+    public void testLenWithEmptyString() {
+        Object result = lenSTD.execute("");
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testLenWithNonEmptyString() {
+        Object result = lenSTD.execute("Hello");
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testLenWithSpecialCharacters() {
+        Object result = lenSTD.execute("!@#$%^&*()");
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testLenWithUnicodeCharacters() {
+        Object result = lenSTD.execute("こんにちは");
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testLenWithNull() {
+        assertThrows(RuntimeException.class, () -> lenSTD.execute((Object[])null));
+    }
+
+    @Test
+    public void testLenWithNoArguments() {
+        assertThrows(RuntimeException.class, () -> lenSTD.execute());
+    }
+
+    @Test
+    public void testLenWithNonStringArgument() {
+        assertThrows(RuntimeException.class, () -> lenSTD.execute(123));
+    }
+}
