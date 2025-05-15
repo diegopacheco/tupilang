1dd49875c757d29aa2bf187cfb0cc5cb2e51b268
diegopacheco <diego.pacheco.it@gmail.com>
2025-05-15 03:26:18 -0700

    improve tupilang - massive java module fix, added contructors, clean up build warnings.: src/main/java/module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.github.diegopacheco.tupilang {
     exports com.github.diegopacheco.tupilang;
-    exports com.github.diegopacheco.tupilang.lexer;
-    exports com.github.diegopacheco.tupilang.token;
-    exports com.github.diegopacheco.tupilang.parser;
-    exports com.github.diegopacheco.tupilang.repl;
     exports com.github.diegopacheco.tupilang.ast;
-    exports com.github.diegopacheco.tupilang.interpreter;
+    exports com.github.diegopacheco.tupilang.lexer;
+    exports com.github.diegopacheco.tupilang.parser;
+    exports com.github.diegopacheco.tupilang.token;
+    exports com.github.diegopacheco.tupilang.repl;
+
     requires java.base;
 }
